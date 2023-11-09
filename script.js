95b149f19de1dc66b7bf2802cb69c8c440761515
Henrique Heron <henriqueheron1@gmail.com>
2023-11-09 06:46:16 -0300

    adapt branch to have main's contents

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-    const displayPlayerChoice = document.querySelector(".display-player-choice");
-    const displayComputerChoice = document.querySelector(".display-computer-choice");
+    const optionButtons = document.querySelectorAll('.option');
+    const playerAnswerDisplay = document.querySelector('.display-player-choice');
+    const computerAnswerDisplay = document.querySelector('.display-computer-choice');
     const result = document.querySelector("#result");
-    const bgMusic = document.querySelector("#background-music");
-    const toggleMusicButton = document.querySelector("#toggle-music-button");
-    let musicPlaying = true;
 
     function getComputerChoice() {
         let computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -19,69 +17,38 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    function playRound() {
-        let computerChoice = getComputerChoice();
-        if (playerChoice === computerChoice) {
-            return 'Draw.';
-        } else if (playerChoice === 'rock' && computerChoice === 'scissors' ||
-        playerChoice === 'scissors' && computerChoice === 'paper' ||
-        playerChoice === 'paper' && computerChoice === 'rock') {
-            return `You win! ${playerChoice} beats ${computerChoice}.`;
-        } else {
-            return `You lose. ${computerChoice} beats ${playerChoice}.`;
-        }
-    }
-
-    function showWinner() {
-        result.textContent = playRound();
-        let playAgainContainer = document.querySelector(".play-again-container");
-        
-        if (!playAgainContainer.querySelector("button")) {
-            let playAgainButton = document.createElement("button");
-            playAgainButton.textContent = 'Play Again';
-            playAgainButton.id = 'play-again';
-            playAgainContainer.appendChild(playAgainButton);
-        }
-            playAgainContainer.querySelector("button").addEventListener('click', () => {
-                showWinner();
-        })
+    // function playRound() {
+    //     let computerChoice = getComputerChoice();
+    //     let playerChoice = 
+    //     if (playerChoice === computerChoice) {
+    //         return 'Draw.';
+    //     } else if (playerChoice === 'rock' && computerChoice === 'scissors' ||
+    //     playerChoice === 'scissors' && computerChoice === 'paper' ||
+    //     playerChoice === 'paper' && computerChoice === 'rock') {
+    //         return `You win! ${playerChoice} beats ${computerChoice}.`;
+    //     } else {
+    //         return `You lose. ${computerChoice} beats ${playerChoice}.`;
+    //     }
+    // }
     
-    }
-
-
-    // User interface starts here
-
 
     // New UI
-    const optionButtons = document.querySelectorAll('.option');
-    const playerP = document.querySelector('.display-player-choice');
-
+    
     optionButtons.forEach(button => {
 
         button.addEventListener('click', (e) => {
-            console.log("Event click happened");
-            switch (e.target.id) {
-                case 'rock':
-                    playerP.textContent = `You played ${e.target.id}.`
-                    break;
-                case 'paper':
-                    playerP.textContent = `You played ${e.target.id}.`
-                    break;
-                case 'scissors':
-                    playerP.textContent = `You played ${e.target.id}.`
-                    break;
-            }
-            let computerChoice = getComputerChoice();
-                switch (computerChoice) {
-                    case 1:
-                return 'rock';
-                    case 2:
-                return 'paper';
-                    case 3:
-                return 'scissors';
-                }
+           console.log(`Player played ${e.target.id}`);
+           getComputerChoice();
+            return e.target.id;
         })
     })
     
+    function showWinner() {
+        let computerChoice = getComputerChoice();
+                computerAnswerDisplay.textContent = `Computer played ${computerChoice}.`
+            let playerChoice = e.target.id;
+            console.log(playerChoice);
+            
+    }
 
 });
